7018afa3aa721d806d43e59125cf2124952c6d4e
joey <folayanjoey@gmail.com>
2023-06-30 00:37:35 +0100

    feature: gravatar avater wrapper

--- avatar.go
+++ avatar.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"crypto/md5"
 	"errors"
+	"fmt"
+	"io"
+	"strings"
 )
 
 // ErrNoAvater is the error that is returned when the avater instance is unable
 // to provide a avater URL.
 var ErrNoAvaterURL = errors.New("chat: Unable to get an avatar URL")
+
+// ErrNoEmailAdrrs is the error that is returned when the avater instance is unable
+// to provide a email URL.
+var ErrNoEmailAdrrs = errors.New("chat: Unable to get an email address")
 
 // Avatar represents types capable of representing user profile pictures
 type Avater interface {
@@ -16,8 +24,10 @@ type Avater interface {
 }
 
 type AuthAvatar struct{}
+type GravatarAvatar struct{}
 
 var UseAuthAvater AuthAvatar
+var UseGravater GravatarAvatar
 
 func (AuthAvatar) GetAvaterURL(c *client) (string, error) {
 	if url, ok := c.userData["avatar_url"]; ok {
@@ -26,4 +36,15 @@ func (AuthAvatar) GetAvaterURL(c *client) (string, error) {
 		}
 	}
 	return "", ErrNoAvaterURL
+}
+
+func (GravatarAvatar) GetAvaterURL(c *client) (string, error) {
+	if email, ok := c.userData["email"]; ok {
+		if emailStr, ok := email.(string); ok {
+			m := md5.New()
+			io.WriteString(m, strings.ToLower(emailStr))
+			return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
+		}
+	}
+	return "", ErrNoEmailAdrrs
 }
--- avater_test.go
+++ avater_test.go
@@ -16,3 +16,18 @@ func TesTAuthAvater(t *testing.T) {
 		t.Error("AuthAvater.GetAvaterURL should return correct URL")
 	}
 }
+
+func TestGravaterAvater(t *testing.T) {
+	var gravaterAvater GravatarAvatar
+	client := new(client)
+	client.userData = map[string]interface{}{
+		"email": "MyEmailAddress@example.com",
+	}
+	url, err := gravaterAvater.GetAvaterURL(client)
+	if err != nil {
+		t.Error("GravatarAvater.GetAvaterURL shouldnt return an error")
+	}
+	if url != "//www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346" {
+		t.Errorf("GravatarAvater.GetAvaterURL shouldnt return %s, which is incorrect", url)
+	}
+}
